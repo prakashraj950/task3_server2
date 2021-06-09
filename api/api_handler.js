10dcfd56a6250f97f38ab69c823276432a42f40f
prakashraj950 <prakash950288@gmail.com>
2021-06-09 18:35:18 +0530

    initial commit

--- api/api_handler.js
+++ api/api_handler.js
@@ -1,30 +1,46 @@
 import express from 'express';
-import{clickUpdate, fetchData, getId,storeData} from '../control/formcnt.js'
+import{clickUpdate, fetchData, getId,storeData,getAdId} from '../control/formcnt.js'
 export default async function installHandler(app){
     
     
     app.use(express.json());
     app.use('/ads',express.static('./ads'));
-    app.post('/ad', async(req, res)=>{
-      try{  
-      const result = await fetchData();
-      const id = await getId(req.body.page_name);
-      const service = {
-        Ad_id: result.id,
-        domain_id: req.body.domain_id,
-        user: req.body.user,
-        ip_address: req.body.ip_address,
-        page_id: id,
-        clicked:0};
-       const service_id = await storeData(service);
-       const file_name = result.file;
-      res.send({file_name,service_id});
-    }
-      
-      catch(err){
-        res.send(err)
+    app.post('/ad', async (req, res) => {
+      try {
+        const domain_id = req.body.domain_id;
+        const page_id = await getId(req.body.page_name);
+        const Ad_id = await getAdId(
+          domain_id,
+          page_id,
+          req.body.age_group,
+          req.body.city
+        );
+        console.log(Ad_id)
+        const ad_data = await fetchData(Ad_id);
+  
+        const service = {
+          Ad_id,
+          domain_id,
+          user: req.body.user,
+          ip_address: req.body.ip_address,
+          page_id, 
+          clicked: 0,
+         age_group: req.body.age_group,
+         city: req.body.city
+
+        };
+        console.log(service);
+        const service_id = await storeData(service);
+        const file_name = ad_data.file;
+        console.log(service_id);
+        
+        res.send({ file_name, service_id });
       }
-      });
+      catch (err) {
+        console.log("could not serve ad", err);
+        res.status(400).send(err);
+      }
+    });
 
       app.post('/click',(req,res)=>{
         console.log(req.body);
--- control/formcnt.js
+++ control/formcnt.js
@@ -1,20 +1,18 @@
 import connectDatabase from '../database/db_connection.js';
-import {FormSetDb} from "../database/formdb.js";
 const con = connectDatabase();
-const form_set_db = new FormSetDb(con);
 
 
-  export  function fetchData(){
-    let stmt = "SELECT * FROM ads";
-    return new Promise ( (resolve, reject) => {
-      con.query(stmt,(err,r)=>{ 
-          if (err) reject(err);
-          else if (r.length !== 0){ const row=r[Math.floor(Math.random() * r.length)]; resolve(row);}
-          else reject();
-        
-        });
-    })
-  }
+
+export function fetchData(id) {
+  let stmt = "SELECT * FROM ads WHERE id = ?";
+  return new Promise( (resolve, reject) => {
+    con.query(stmt, [id], (err, r) => {
+      if (err) reject(err);
+      else if (r.length === 1) resolve(r[0]);
+      else reject("invalid data in ads table");
+    });
+  });
+}
   export  function getId(page_name){
     let stmt = "SELECT id FROM page WHERE STRCMP (name, ?)=0"
     return new Promise ( (resolve, reject) => {
@@ -43,5 +41,34 @@ export async function clickUpdate(id){
     con.query(stmt,[id],(err,r)=>{
       if (err) throw err;
     })
+}
+
+export function getAdId(domain, page, age_group, city) {
+  let stmt = `SELECT ad_custom.Ad_id AS id
+                FROM ad_label
+                INNER JOIN ad_custom
+                ON ad_label.Ad_id = ad_custom.Ad_id
+              WHERE ad_custom.Domain_id = ?
+                AND ad_custom.page_id = ?`;
+  const data = [domain,  page];
+  if (age_group !== null && typeof age_group !== "undefined") {
+    stmt += "\nAND ad_label.age_group = ?";
+    data.push(age_group);
+  }
+  if (city !== null && typeof city !== "undefined") {
+    stmt += "\nAND STRCMP(ad_label.city, ?) = 0";
+    data.push(city);
+  }
+  
+  return new Promise( (resolve, reject) => {
+    con.query(stmt, data, (err, r) => {
+      if (err) reject(err);
+      else if (r.length !== 0) {
+        const row = r[Math.floor(Math.random() * r.length)];
+        resolve(row.id);
+      }
+      else reject("no ad exists");
+    });
+  });
 }
   
