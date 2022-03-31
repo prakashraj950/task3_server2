bcfc7eb3e043dc9c45c3990a443dfd221bba95cb
prakashraj950 <prakash950288@gmail.com>
2022-03-31 13:38:53 +0530

    pus

--- database/db_connection.js
+++ database/db_connection.js
@@ -4,18 +4,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
     host: process.env.DATABASE_HOST ||'remotemysql.com',
     user: process.env.DATABASE_USERNAME||'TvYOQvumBU',
     password: process.env.DATABASE_PASSWORD||'odhayQ9Kz8',
     database: process.env.DATABASE_NAME || 'TvYOQvumBU'
 })
 
-con.connect((err)=>{
-    if(!err){
-        console.log('Connected');
-    }else console.log('conection Failed',err)
-})
+// con.connect((err)=>{
+//     if(!err){
+//         console.log('Connected');
+//     }else console.log('conection Failed',err)
+// })
 
 export default function connectDatabase(){
     // con.connect((err) => {
