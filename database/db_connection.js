449de884b5b3c13ed95f38e299a132c203f31894
prakashraj950 <prakash950288@gmail.com>
2022-03-31 12:38:19 +0530

    db cange

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,3 +1,4 @@
+
 import mysql from 'mysql';
 import dotenv from 'dotenv';
 
@@ -13,7 +14,7 @@ const con = mysql.createConnection({
 con.connect((err)=>{
     if(!err){
         console.log('Connected');
-    }else console.log('conection Failed')
+    }else console.log('conection Failed',err)
 })
 
 export default function connectDatabase(){
@@ -23,3 +24,51 @@ export default function connectDatabase(){
     // });
     return con
 }
+
+
+
+
+// let db_config = {
+//   connectionLimit: 200,
+//   host: process.env.DATABASE_HOST ||'remotemysql.com',
+//   database: process.env.DATABASE_NAME || 'TvYOQvumBU',
+//   user: process.env.DATABASE_USERNAME||'TvYOQvumBU',
+//   password: process.env.DATABASE_PASSWORD||'odhayQ9Kz8',
+//   // timeout: 1000
+// };
+// let db_config1 = {
+//   connectionLimit: 10,
+//   host: process.env.DATABASE_HOST ||'remotemysql.com',
+//   database: process.env.DATABASE_NAME || 'TvYOQvumBU',
+//   user: process.env.DATABASE_USERNAME||'TvYOQvumBU',
+//   password: process.env.DATABASE_PASSWORD||'odhayQ9Kz8',
+//   timeout: 1000
+// };
+// var connection;
+
+// function connectDatabase() {
+//   connection = mysql.createConnection(db_config); // Recreate the connection, since
+//                                                   // the old one cannot be reused.
+
+//   connection.connect(function(err) {              // The server is either down
+//     if(err) {                                     // or restarting (takes a while sometimes).
+//       console.log('error when connecting to db:', err);
+//       setTimeout(connectDatabase, 2000); // We introduce a delay before attempting to reconnect,
+//     }                                     // to avoid a hot loop, and to allow our node script to
+//   });                                     // process asynchronous requests in the meantime.
+//                                           // If you're also serving http, display a 503 error.
+//   connection.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//       connectDatabase();                         // lost due to either server restart, or a
+//     } else {                                      // connnection idle timeout (the wait_timeout
+//       throw err;                                  // server variable configures this)
+//     }
+//   });
+//   return connection;
+// }
+// connectDatabase();
+
+// export default  connectDatabase
+ 
+  
